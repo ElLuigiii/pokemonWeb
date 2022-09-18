79fe73d3823dafd270bff3eb002cf053f086c790
ElLuigiii <luiisalvarez2018@gmail.com>
2022-09-18 16:19:18 -0300

    set-up-1: Modified code, remove child function, and refresh page buttons added.

--- index.js
+++ index.js
@@ -1,71 +1,85 @@
-for (let index = 1; index <= 10; index++) {
-    fetch( `https://pokeapi.co/api/v2/pokemon/${index}`)
-    .then(res => res.json())
-    .then( data => crearTarjetaPokemon(data))
-    .catch (error => console.log(error));
-}
-//hacer pokemon es la funcion que quiero hacer para traer todos los pokemon de una.
-// primero hacer un fetch a la api
-//despues hacer un for a el resultado de la api trayendo los datos
-const urlApi= 'https://pokeapi.co/api/v2/pokemon/'
-
-//ahora lo que voy a hacer es una funcion que añade clase al tipo de pokemon
-//recibe un dato y debido al dato que recibe depende la clase que añade
-//el dato que recibe es el tipo de pokemon que lo recibimos de la promesa
-//luego esta funcion la ponemos al final de la funcion de crear tarjeta
-//y al final de la funcion buscar pokemon
-
-function typeOfPokemon (data) {
-        // aca voy a agregarle una clase al logo del typo de pokemon
-        //dependiendo del tipo
-    if(data === 'fire') {
-        console.log('ahi va saliendo')
-        const typeLogo = document.getElementsByClassName('pokemonType')
-        //typeLogo.classList.add('element')
-    }else console.log('talvez sea un = menos')
-}
-
-function crearTarjetaPokemon(pokemon) {
-    const container = document.getElementById('contenedor')
-    //aca va la funcion de crear tarjeta
-    const tarjeta = document.createElement('div')
-    tarjeta.classList.add('container-img')
-    //y poner clase al crear elemento
-    //vamos a crear las clases para el css
-    const nameToPokemon = document.createElement('h1')
-    nameToPokemon.classList.add('namePokemon')
-    // aca tengo que hacer la varibale del nombre y paasar la primera letra a mayuscula
-    const letterPokemon = pokemon.name
-    const firstLetter = letterPokemon.slice(0,1).toUpperCase()
-    const othersLetters = letterPokemon.slice(1)
-    //aca va a ir el tipo de pokemon
-    const pokemonType= document.createElement('p')
-    pokemonType.classList.add('pokemonType')
-    pokemonType.textContent = pokemon.types[0].type.name
-
-    //aca va el contenido del nombre del pokemon
-    nameToPokemon.textContent = firstLetter + othersLetters
-    const img = document.createElement('img')
-    img.classList.add('img-pokemon')
-    img.src = pokemon.sprites.other.dream_world.front_default
-
-    tarjeta.appendChild(nameToPokemon)
-    tarjeta.appendChild(pokemonType)
-    tarjeta.appendChild(img)
-    container.appendChild(tarjeta)
-}
+let btnPrevious
+let btnSiguiente
+let templateHTML
+const buttons = document.getElementById('buttons')
 const container = document.getElementById('contenedor')
+const urlApi= 'https://pokeapi.co/api/v2/pokemon/'
 const botonNuevo = document.getElementById('btn_buscar')
 const inputBuscar = document.getElementById('input_nav')
-const url = 'https://pokeapi.co/api/v2/pokemon/'
+
+const ImprimirBotones = (data) => {
+    let boton_container = document.getElementById('buttons')
+    if(data.next && data.previous){
+        btnNext= `<button class="boton_Nav" data-url=${data.next}>Siguiente</button>`
+        btnPrevious=`<button class="boton_Nav" data-url=${data.previous}>Anterior</button>`
+        return boton_container.innerHTML+=btnPrevious+btnNext
+    }else if (!data.previous){
+        btnNext= `<button class="boton_Nav" data-url=${data.next}>Siguiente</button>`
+        btnPrevious=`<button class="boton_Nav" data-url=${data.previous}>Anterior</button>`
+        return boton_container.innerHTML+=btnNext
+    }else return boton_container.innerHTML+=btnPrevious
+}
+//funcion para eliminar botones
+function removeChild (contenedorPadre) {
+    while (contenedorPadre.firstChild) {
+        contenedorPadre.removeChild(contenedorPadre.firstChild)
+    }
+}
+const ImprimirPokemons = async (data) => {
+    container.innerHTML= ''
+    try{    data.forEach(async element => {
+            const direccion = await fetch(element.url)
+            const resultado = await direccion.json()
+            //const cumplido = await response.json()
+            //console.log(resultado)
+            //console.log(cumplido)
+            templateHTML =`
+            <div class="container-img">
+            <h1 class"namePokemon>${resultado.name}</h1>
+            <p class"pokemonType">${resultado.types[0].type.name}</p>
+            <img class"img-pokemon" src=${resultado.sprites.other.dream_world.front_default} alt="${resultado.name}"/>
+            </div>
+            `
+            container.innerHTML+=templateHTML
+        });
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+const GetPokemons = async (url) => {
+    try {
+        const response = await fetch(url)
+        const result = await response.json()
+        console.log(result)
+        removeChild(buttons)
+        ImprimirBotones(result)
+        ImprimirPokemons(result.results)
+        // btnSiguiente =result.next ? `<button class="boton_Nav" data-url=${result.next}>Siguiente</button>` : ''
+        // btnPrevious= result.previous ? `<button class="boton_Nav" data-url=${result.previous}>Anterior</button>` :''
+        // buttons.innerHTML+= btnSiguiente + ' ' + btnPrevious
+        //por aca una fucnion que recorra el array de resul.results
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+GetPokemons(urlApi)
+buttons.addEventListener('click', (e) =>{
+    if(e.target.classList.contains('boton_Nav')){
+        let value = e.target.dataset.url
+        GetPokemons(value)
+        console.log(value)
+    }
+})
 
 function buscarPokemon() {
-    fetch(`${url}${inputBuscar.value}`)
+    fetch(`${urlApi}${inputBuscar.value}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         const container = document.getElementById('contenedor')
-
+        removeChild(container)
         const onePokemon = document.createElement('div')
         onePokemon.classList.add('container-img')
 
@@ -77,7 +91,6 @@ function buscarPokemon() {
         const pokemonType= document.createElement('p')
         pokemonType.classList.add('pokemonType')
         pokemonType.textContent = data.types[0].type.name
-
         const imgPokemon = document.createElement('img')
         imgPokemon.classList.add('img-pokemon')
         imgPokemon.src = data.sprites.other.dream_world.front_default
@@ -85,10 +98,12 @@ function buscarPokemon() {
         onePokemon.appendChild(pokemonType)
         onePokemon.appendChild(imgPokemon)
         container.appendChild(onePokemon)
+        //console.log(data.types.length)
         //type of pokemon es la funcion que agrega una clase al distintiva al tipo de pokemon
-        let atributoApi = data.types[0].type.name
-        typeOfPokemon(atributoApi)
+        //typeOfPokemon(atributoApi)
     })
     .catch (error => console.log(error))
 }
 botonNuevo.addEventListener('click', buscarPokemon)
+
+
